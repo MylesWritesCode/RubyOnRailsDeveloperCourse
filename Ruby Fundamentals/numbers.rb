d408210a349386f535f577d9bfc96edc94067037
Myles Berueda <myles.berueda@gmail.com>
2017-01-05 13:08:35 -1000

    Completed Numbers lesson

---
+++ Ruby Fundamentals/numbers.rb
@@ -0,0 +1,10 @@
+puts "Please enter a number: "
+first_num = gets.chomp.to_f
+puts "Please enter a second number: "
+second_num = gets.chomp.to_f
+puts "The first number multiplied by the second number is #{first_num * second_num}"
+
+# Homework Assignment
+puts "The first number divided by the second number is #{first_num / second_num}"
+puts "The first number subtracted by the second number is #{first_num - second_num}"
+puts "The remainder of the first number divided by the second number is #{first_num % second_num}"
