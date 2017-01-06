0b96b4c7563f2bb777a452cdc285d8f45a41fcde
Myles Berueda <myles.berueda@gmail.com>
2017-01-05 14:37:22 -1000

    Made parameters in numbersMethod.rb make sense

--- Ruby Fundamentals/numbersMethod.rb
+++ Ruby Fundamentals/numbersMethod.rb
@@ -1,13 +1,13 @@
 def arithmetic(x, y, method)
-  if (method == "*")
+  if (method == "multiply")
     return x * y
-  elsif (method == "/")
+  elsif (method == "divide")
     return x / y
-  elsif (method == "+")
+  elsif (method == "add")
     return x + y
-  elsif (method == "-")
+  elsif (method == "subtract")
     return x - y
-  elsif (method == "%")
+  elsif (method == "remainder")
     return x % y
   end
 end
@@ -16,8 +16,8 @@ puts "Please enter a number for x: "
 x = gets.chomp.to_f
 puts "Please enter a number for y: "
 y = gets.chomp.to_f
-puts "X times Y is #{arithmetic(x, y, '*')}"
-puts "X divided by Y is #{arithmetic(x, y, '/')}"
-puts "X plus Y is #{arithmetic(x, y, '+')}"
-puts "X minus Y is #{arithmetic(x, y, '-')}"
-puts "The remainder of X divided by Y is #{arithmetic(x, y, '%')}"
+puts "X times Y is #{arithmetic(x, y, 'multiply')}"
+puts "X divided by Y is #{arithmetic(x, y, 'divide')}"
+puts "X plus Y is #{arithmetic(x, y, 'add')}"
+puts "X minus Y is #{arithmetic(x, y, 'subtract')}"
+puts "The remainder of X divided by Y is #{arithmetic(x, y, 'remainder')}"
