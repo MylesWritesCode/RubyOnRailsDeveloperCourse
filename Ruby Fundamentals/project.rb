7d337056cc5cb546e47002f541a03dabe259cc4e
Myles Berueda <myles.berueda@gmail.com>
2017-01-06 09:52:53 -1000

    Cleanup
    Moved project.rb to it's own directory and renamed to user.rb.

--- Ruby Fundamentals/project.rb
+++
@@ -1,4 +0,0 @@
-# Reads user information that is provided
-# Reads permissions from a json file
-# Creates a new user with assigned permissions
-# Saves the new user in a new users file. Create one if none exists.
