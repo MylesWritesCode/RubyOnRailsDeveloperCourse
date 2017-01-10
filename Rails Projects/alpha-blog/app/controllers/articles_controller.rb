9cf69190386c410c8188fdeabefaf6b6b3d1ef3c
Myles Berueda <myles.berueda@gmail.com>
2017-01-10 00:00:38 -1000

    Updated flash messages for errors.

--- Rails Projects/alpha-blog/app/controllers/articles_controller.rb
+++ Rails Projects/alpha-blog/app/controllers/articles_controller.rb
@@ -15,7 +15,9 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
     else
-      flash[:warning] = Article.errors.full_messages
+      @article.errors.full_messages.each do |msg|
+        flash[:warning] = msg
+      end
       render :new
     end
   end
