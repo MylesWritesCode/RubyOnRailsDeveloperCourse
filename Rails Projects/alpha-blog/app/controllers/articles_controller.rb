9af6935e7bedf235fe7b2fd2ddf1ec225f30a4b3
Myles Berueda <myles.berueda@gmail.com>
2017-01-10 15:18:54 -1000

    Added more styles to form.

--- Rails Projects/alpha-blog/app/controllers/articles_controller.rb
+++ Rails Projects/alpha-blog/app/controllers/articles_controller.rb
@@ -15,9 +15,7 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
     else
-      @article.errors.full_messages.each do |msg|
-        flash[:warning] = msg
-      end
+      flash[:danger] = "The article could not be saved."
       render :new
     end
   end
