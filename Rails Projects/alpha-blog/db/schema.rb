fb74805ff6c53e06445f1509f1ecec8041d8f069
Myles Berueda <myles.berueda@gmail.com>
2017-01-13 11:52:45 -1000

    Added admin to users table.

--- Rails Projects/alpha-blog/db/schema.rb
+++ Rails Projects/alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112231408) do
+ActiveRecord::Schema.define(version: 20170113213028) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20170112231408) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
