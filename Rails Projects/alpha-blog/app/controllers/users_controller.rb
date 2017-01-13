42f37cbb2ecde830ce729216c912962bff2439e9
Myles Berueda <myles.berueda@gmail.com>
2017-01-13 11:21:18 -1000

    Added errors to users controller.

--- Rails Projects/alpha-blog/app/controllers/users_controller.rb
+++ Rails Projects/alpha-blog/app/controllers/users_controller.rb
@@ -13,9 +13,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha Blog, #{@user.username}!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
+      @user.errors.full_messages.each do |msg|
+        flash[:danger] = msg
+      end
       render 'new'
     end
   end
