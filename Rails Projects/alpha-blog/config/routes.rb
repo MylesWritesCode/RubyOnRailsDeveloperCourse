d475864e5aeef1556ae50ecbad9b5e53bae2ce2a
Myles Berueda <myles.berueda@gmail.com>
2017-01-12 18:08:36 -1000

    Added sessions for users.
    I've added users authentication (login/logout), a session id, the views, controller actions, and paths. I've also rounded it off by adding links to the other pages to lead to session views.

--- Rails Projects/alpha-blog/config/routes.rb
+++ Rails Projects/alpha-blog/config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  # START sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  # END sessions
   resources :articles
 end
