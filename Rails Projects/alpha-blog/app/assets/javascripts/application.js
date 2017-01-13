e7d2ed753a5e2fbfbee2b7790be4fbb954ed1728
Myles Berueda <myles.berueda@gmail.com>
2017-01-12 17:03:15 -1000

    Added endless scrolling.
    Using gem 'will_paginate' a some jQuery, I was able to create an endless scrolling page on the articles listing. The jQuery is based on some code from Rich on Rails.

--- Rails Projects/alpha-blog/app/assets/javascripts/application.js
+++ Rails Projects/alpha-blog/app/assets/javascripts/application.js
@@ -14,4 +14,19 @@
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require_self
 //= require_tree .
+
+// For Endless Pagination
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
