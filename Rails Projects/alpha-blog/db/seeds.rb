55ed23f1d33ae5ae6cf8af30abc50d4f93ad35ba
Myles Berueda <myles.berueda@gmail.com>
2017-01-12 17:26:15 -1000

    Added endless scrolling for users index.

--- Rails Projects/alpha-blog/db/seeds.rb
+++ Rails Projects/alpha-blog/db/seeds.rb
@@ -5,13 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-(1..30).each do |i|
-  Article.create!(title: "Lipsum Post #{i}", description: "This is the #{i}th post.", body: %{
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat purus dapibus fermentum sagittis. Fusce in tempus felis. Phasellus a erat ut lorem lacinia bibendum. Vivamus viverra facilisis neque, in scelerisque urna pharetra vel. Donec a est mauris. Integer eget metus quis eros egestas elementum. Integer bibendum risus hendrerit dapibus tempor. Fusce placerat in orci vitae tincidunt. }, user_id: "1")
-end
-
-(1..30).each do |i|
-  Article.create!(title: "Lipsum Post #{i}", description: "This is the #{i}th post.", body: %{
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat purus dapibus fermentum sagittis. Fusce in tempus felis. Phasellus a erat ut lorem lacinia bibendum. Vivamus viverra facilisis neque, in scelerisque urna pharetra vel. Donec a est mauris. Integer eget metus quis eros egestas elementum. Integer bibendum risus hendrerit dapibus tempor. Fusce placerat in orci vitae tincidunt. }, user_id: "2")
-end
