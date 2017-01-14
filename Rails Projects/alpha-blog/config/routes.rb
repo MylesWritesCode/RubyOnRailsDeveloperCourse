965b6873051431a052a688b9e7681a24d5bac3f6
Myles Berueda <myles.berueda@gmail.com>
2017-01-13 16:12:58 -1000

    Started controller testing categories.

--- Rails Projects/alpha-blog/config/routes.rb
+++ Rails Projects/alpha-blog/config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   # END sessions
   resources :articles
+  resources :categories, except: [:destroy]
 end
--- Rails Projects/alpha-blog/test/models/category_test.rb
+++ Rails Projects/alpha-blog/test/models/category_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-
   def setup
     @category = Category.new(name: "sports")
   end
@@ -30,5 +29,4 @@ class CategoryTest < ActiveSupport::TestCase
     @category.name = "ab"
     assert_not @category.valid?
   end
-
 end
