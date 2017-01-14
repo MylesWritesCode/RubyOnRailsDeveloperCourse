4513ffee1cffc9f3a419d33a501795855ac87b16
Myles Berueda <myles.berueda@gmail.com>
2017-01-13 18:30:55 -1000

    Complete category integration testing.

--- Rails Projects/alpha-blog/app/controllers/categories_controller.rb
+++ Rails Projects/alpha-blog/app/controllers/categories_controller.rb
@@ -1,15 +1,33 @@
 class CategoriesController < ApplicationController
 
   def index
-
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category created!"
+      redirect_to categories_path
+    else
+      @category.errors.full_messages.each do |msg|
+        flash[:danger] = msg
+      end
+      render 'new'
+    end
   end
 
   def show
 
   end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
