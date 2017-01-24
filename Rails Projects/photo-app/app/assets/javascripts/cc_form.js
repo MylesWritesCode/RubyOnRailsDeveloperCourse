b7ca61a0cdb5222f540b3313dddea7a7ca2e8e45
Myles Berueda <myles.berueda@gmail.com>
2017-01-24 04:25:56 -1000

    More debugging with Stripe payments.
    Production has not been good to me. Changed up some syntax, cleaned up, and added fallbacks to append in cc_form.js.

--- Rails Projects/photo-app/app/assets/javascripts/cc_form.js
+++ Rails Projects/photo-app/app/assets/javascripts/cc_form.js
@@ -1,14 +1,18 @@
-/* Stripe */
+/* global $, Stripe */
 $(document).ready(function(){
 
+  // Set Stripe Publishable Key
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+
   $(function() {
-    var $form = $('#cc-form');
-    $form.submit(function(event) {
+    var form = $('#cc-form');
+    $("#btnSignUpSubmit").click(function(event) {
       // Disable the submit button to prevent repeated clicks:
-      $form.find('#btnSignUpSubmit').prop('disabled', true);
+      event.preventDefault();
+      form.find('#btnSignUpSubmit').prop('disabled', true).val("Processing...");
 
       // Request a token from Stripe:
-      Stripe.card.createToken($form, stripeResponseHandler);
+      Stripe.card.createToken(form, stripeResponseHandler);
 
       // Prevent the form from being submitted:
       return false;
@@ -17,13 +21,13 @@ $(document).ready(function(){
 
   function stripeResponseHandler(status, response) {
     // Grab the form:
-    var $form = $('#cc-form');
+    var form = $('#cc-form');
 
     if (response.error) { // Problem!
 
       // Show the errors on the form:
-      $form.find('#errors').addClass("alert-danger").text(response.error.message);
-      $form.find('#btnSignUpSubmit').prop('disabled', false); // Re-enable submission
+      form.find('#errors').addClass("alert-danger").text(response.error.message);
+      form.find('#btnSignUpSubmit').prop('disabled', false).val("Submit"); // Re-enable submission
 
     } else { // Token was created!
 
@@ -31,10 +35,12 @@ $(document).ready(function(){
       var token = response.id;
 
       // Insert the token ID into the form so it gets submitted to the server:
-      $form.append($('<input type="hidden" name="user[stripeToken]">').val(token));
+      form.append($('<input type="hidden" name="stripeToken" id="stripeToken">').val(token));
 
-      // Submit the form:
-      $form.get(0).submit();
+      if ($("#stripeToken").length >= 1) {
+        // Submit the form:
+        form.get(0).submit();
+      }
     }
   };
 });
